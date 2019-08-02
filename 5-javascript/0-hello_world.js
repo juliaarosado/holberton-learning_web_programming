144809f368709176a92fe1f5f4364c48a0a0c404
juliaarosado <53441209+juliaarosado@users.noreply.github.com>
2019-08-02 10:40:20 -0400

    Create 0-hello_world.js

---
+++ 5-javascript/0-hello_world.js
@@ -0,0 +1,12 @@
+VM83:1 Console was cleared
+undefined
+2+2
+4
+"Hello world"
+"Hello world"
+console.log("Hello world")
+VM246:1 Hello world
+undefined
+console.log("Betty Holberton")
+VM317:1 Betty Holberton
+undefined
